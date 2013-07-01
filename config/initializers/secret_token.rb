c05246a2172ff128d0f30552212ba7298cbcf6f0
Shahidrox <rox.shahid@gmail.com>
2013-07-01 20:17:59 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anjscaff::Application.config.secret_token = '956b2feb87bea5921a86e7e8ee693c32f81d89a70a8a80fb16ca9b2277cf4c9fd6518e7005b80c7f9f1f775d42b18adfff93846fb0f5620227c2b7ca0ada96be'
